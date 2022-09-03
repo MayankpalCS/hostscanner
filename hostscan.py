84c58e1959468392fabb92725e7e17b97c699cf0
MayankPal <110147408+MayankpalCS@users.noreply.github.com>
2022-09-03 11:35:06 +0530

    Create hostscan.py

---
+++ hostscan.py
@@ -0,0 +1,49 @@
+from cmath import pi
+from queue import Queue
+import time
+from colorama import init, Fore
+import queue
+import threading
+from urllib.parse import non_hierarchical
+from pythonping import ping
+red = Fore.RED
+green = Fore.GREEN
+yellow = Fore.YELLOW
+reset = Fore.RESET
+def banner():
+    print(f"""{green}
+           |    | |----| |------ --------   ------- |------- |-----| ||     | ||     | |----- |-----|
+           |    | |    | |           |      |       |        |     | | |    | | |    | |      |     |
+           |----| |    | |-----|     |      |       |        |-----| |  |   | |  |   | |----- |-----|
+           |    | |    |       |     |      |-----| |        |     | |   |  | |   |  | |      ||
+           |    | |----| ------|     |            | |        |     | |    | | |    | | |      | |
+                                     l       -----| |------- |     | |     || |     || |----- |  |
+                  developer-@mayankpal                                                            |
+                  github:github.com/mayankpalcs                                                   
+                          
+                           """)                                                                   
+
+
+banner()
+init()
+
+host=input('Enter three block of the subnet')
+threads=int(input('Number of threads recommended(50 or 100)'))
+q = queue.Queue()
+
+
+for i in range(1,226):
+    ip=host+'.'+str(i)
+    q.put(ip)
+def pinging(n,p):
+    while not q.empty():
+        ips=q.get()
+        a=ping(ips, verbose=False,count=2)
+        if a.packet_loss==0:
+            print(f"[+]{green}{ips} host is up")
+        else:
+            pass
+
+for i in range(threads):
+    t=threading.Thread(target=pinging,args=(i,q))
+    t.start()
